5f3b1237232da8fb468efede0311af6ab1d1022c
iskilled <iskilled@gmail.com>
2018-01-22 19:50:22 +0000

    remove email from user (maybe add later)

--- server/src/db/user.js
+++ server/src/db/user.js
@@ -2,7 +2,7 @@ import {thinky} from './thinky';
 
 // Create a model - the table is automatically created
 export const User = thinky.createModel('User', {
-  email: thinky.type.string().required(),
+  // email: thinky.type.string().required(),
   login: thinky.type.string().required(),
   password: thinky.type.string().required(),
   registrationDate: thinky.type.date().default(thinky.r.now()),
