09ed38f1fcbbe18a202068e0ae58c54303730ff2
iskilled <iskilled@gmail.com>
2018-01-23 23:44:49 +0000

    fix reqlite spawn and killing during testing

--- server/test/index.js
+++ server/test/index.js
@@ -2,7 +2,18 @@
 // say we're testing to evade excessive logging
 // usage of process.env is workaround for issues with setting env vars in windows
 process.env.NODE_ENV = 'testing';
+
+const spawn = require('child_process').spawn;
+
+// reqlite instance
+const reqlite = spawn('reqlite');
+
+// wait for start
+reqlite.stderr.on('data', () => {
+  // require and start main tests
+  const startTests = require('./main').default;
+  startTests(reqlite);
+});
+
 // require babel require hook
 require('babel-core/register');
-// require and start main tests
-require('./main');
