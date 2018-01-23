e2e925e017ec1df2e85242f5d155e0b6db378c62
iskilled <iskilled@gmail.com>
2018-01-23 19:52:00 +0000

    disallow registration of multiple users with same login

--- server/src/auth/register.js
+++ server/src/auth/register.js
@@ -2,8 +2,14 @@
 import {User} from '../db';
 import {hash, asyncRequest} from '../util';
 
+export const loginTaken = async(login) => {
+  // check if login is already taken
+  const users = await User.filter({login}).run();
+  return users.length > 0;
+};
+
 export default (app) => {
-  app.post('/api/register', async(req, res) => {
+  app.post('/api/register', asyncRequest(async(req, res) => {
     // get user input
     const {login, password, passwordRepeat} = req.body;
 
@@ -11,9 +17,17 @@ export default (app) => {
       res.status(400).send({error: 'Passwords do not match!'});
       return;
     }
-
+    // hash password
     const hashedPassword = hash(password);
 
+    // check if login already taken
+    const exists = loginTaken(login);
+    if (exists) {
+      res.status(403).send({error: 'User is already registered!'});
+      return;
+    }
+
+    // save new user
     const user = new User({
       login,
       password: hashedPassword,
@@ -21,5 +35,5 @@ export default (app) => {
     await user.save();
 
     res.sendStatus(201);
-  });
+  }));
 };
