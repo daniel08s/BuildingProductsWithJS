6a4eb4f0432b3ac2525c336c4ca4462394657e8d
iskilled <iskilled@gmail.com>
2018-01-27 21:41:44 +0000

    add tests for User update route

--- server/test/user.js
+++ server/test/user.js
@@ -56,4 +56,43 @@ export default (test) => {
         t.end();
       });
   });
+  
+  test('POST /api/user/:id - should only allow changes on own profile', (t) => {
+    request(app)
+      .post('/api/user/1234')
+      .set('x-access-token', app.get('token'))
+      .send({login: 'test123'})
+      .expect(403)
+      .expect('Content-Type', /json/)
+      .end((err, res) => {
+        const expectedBody = {error: 'Not enough permissions to change other user profile!'};
+        const actualBody = res.body;
+
+        t.error(err, 'No error');
+        t.deepEqual(actualBody, expectedBody, 'Get correct error');
+        t.end();
+      });
+  });
+  
+  test('POST /api/user/:id - update with new login', (t) => {
+    request(app)
+      .post(`/api/user/${app.get('user').id}`)
+      .set('x-access-token', app.get('token'))
+      .send({login: 'test123'})
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .end((err, res) => {
+        const expectedBody = {
+          ...app.get('user'),
+          login: 'test123',
+        };
+        const actualBody = res.body;
+
+        t.error(err, 'No error');
+        t.deepEqual(actualBody, expectedBody, 'Retrieve new user');
+        t.notOk(actualBody.password, 'No password included');
+        t.end();
+      });
+  });
+
 };
