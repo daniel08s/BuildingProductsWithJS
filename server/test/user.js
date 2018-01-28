ddde8a0405caa2f023b4d6565e2ad13ff5592136
iskilled <iskilled@gmail.com>
2018-01-28 17:56:09 +0000

    add more test for User update route

--- server/test/user.js
+++ server/test/user.js
@@ -1,10 +1,10 @@
 // npm packages
 import request from 'supertest';
-// import jwt from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
 
 // our packages
 import app from '../src/app';
-// import {auth as authConfig} from '../config';
+import {auth as authConfig} from '../config';
 
 export default (test) => {
   test('GET /api/user/:id', (t) => {
@@ -56,7 +56,7 @@ export default (test) => {
         t.end();
       });
   });
-  
+
   test('POST /api/user/:id - should only allow changes on own profile', (t) => {
     request(app)
       .post('/api/user/1234')
@@ -73,7 +73,60 @@ export default (test) => {
         t.end();
       });
   });
-  
+
+  test('POST /api/user/:id - update with same data', (t) => {
+    request(app)
+      .post(`/api/user/${app.get('user').id}`)
+      .set('x-access-token', app.get('token'))
+      .send({login: 'test', password: '123'})
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .end((err, res) => {
+        const expectedBody = app.get('user');
+        const actualBody = res.body;
+
+        t.error(err, 'No error');
+        t.deepEqual(actualBody, expectedBody, 'Retrieve new user');
+        t.notOk(actualBody.password, 'No password included');
+        t.end();
+      });
+  });
+
+  test('POST /api/user/:id - should throw error if new passwords do not match', (t) => {
+    request(app)
+      .post(`/api/user/${app.get('user').id}`)
+      .set('x-access-token', app.get('token'))
+      .send({password: '12345', passwordRepeat: '123'})
+      .expect(400)
+      .expect('Content-Type', /json/)
+      .end((err, res) => {
+        const expectedBody = {error: 'Passwords do not match!'};
+        const actualBody = res.body;
+
+        t.error(err, 'No error');
+        t.deepEqual(actualBody, expectedBody, 'Retrieve new user');
+        t.end();
+      });
+  });
+
+  test('POST /api/user/:id - update with existing login', (t) => {
+    request(app)
+      .post(`/api/user/${app.get('user').id}`)
+      .set('x-access-token', app.get('token'))
+      .send({login: 'notthesame'})
+      .expect(400)
+      .expect('Content-Type', /json/)
+      .end((err, res) => {
+        const expectedBody = {error: 'Login is already taken!'};
+        const actualBody = res.body;
+
+        t.error(err, 'No error');
+        t.deepEqual(actualBody, expectedBody, 'Retrieve new user');
+        t.notOk(actualBody.password, 'No password included');
+        t.end();
+      });
+  });
+
   test('POST /api/user/:id - update with new login', (t) => {
     request(app)
       .post(`/api/user/${app.get('user').id}`)
@@ -95,4 +148,51 @@ export default (test) => {
       });
   });
 
+  test('POST /api/user/:id - update with new password', (t) => {
+    request(app)
+      .post(`/api/user/${app.get('user').id}`)
+      .set('x-access-token', app.get('token'))
+      .send({password: 'qwe', passwordRepeat: 'qwe'})
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .end((err, res) => {
+        const expectedBody = {
+          ...app.get('user'),
+          login: 'test123',
+        };
+        const actualBody = res.body;
+
+        t.error(err, 'No error');
+        t.deepEqual(actualBody, expectedBody, 'Retrieve new password');
+        t.notOk(actualBody.password, 'No password included');
+        t.end();
+      });
+  });
+  
+  test('Should login with updated username and password', (t) => {
+    request(app)
+      .post('/api/login')
+      .send({login: 'test123', password: 'qwe'})
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .end((err, res) => {
+        const actualBody = res.body;
+
+        t.error(err, 'No error');
+        t.ok(actualBody.user, 'User exists');
+        t.ok(actualBody.token, 'Token exists');
+
+        const decodedUser = jwt.verify(actualBody.token, authConfig.jwtSecret);
+        delete decodedUser.iat;
+
+        t.equal(actualBody.user.login, 'test123', 'Login matches request');
+        t.notOk(actualBody.user.password, 'No password included');
+        t.deepEqual(actualBody.user, decodedUser, 'User must match token');
+
+        app.set('token', actualBody.token);
+        app.set('user', actualBody.user);
+
+        t.end();
+      });
+  });
 };
