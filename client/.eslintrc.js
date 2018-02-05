da9455fe2bda26d71b73776ad1096011e2997b07
iskilled <iskilled@gmail.com>
2018-02-05 19:35:41 +0000

    Set up basic client project with webpack, babel and react

---
+++ client/.eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  "parser": "babel-eslint",
+  "extends": "airbnb",
+  "env": {
+    "browser": true
+  },
+  "plugins": [
+    "babel"
+  ],
+  "rules": {
+    "object-curly-spacing": ["warn", "never"],
+    "func-names": "off",
+    "space-before-function-paren": ["error", "never"],
+    "max-len": ["error", 120, 4],
+    "no-unused-vars": ["error", {"argsIgnorePattern": "next"}],
+    "import/prefer-default-export": "off",
+    "comma-dangle": ["error", {
+      "arrays": "always-multiline",
+      "objects": "always-multiline",
+      "functions": "ignore"
+    }],
+    "import/no-extraneous-dependencies": [
+    "error", {
+       "devDependencies": true, 
+       "optionalDependencies": false, 
+       "peerDependencies": false, 
+       "packageDir": "./"
+    }],
+    "no-unused-expressions": 0,
+    "babel/no-unused-expressions": 2,
+    "react/jsx-filename-extension": "off"
+  }
+};
