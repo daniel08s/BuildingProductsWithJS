c72dac11574fb6cf6810566f6186c94fea435834
iskilled <iskilled@gmail.com>
2018-02-05 19:42:23 +0000

    add debug serve script and use it in npm start

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = (
+const App = () => (
   <h1>Hello, World!</h1>
 );
 
 const div = document.getElementById('app');
 
 // render on page
-ReactDOM.render(App, div);
+ReactDOM.render(<App />, div);
