6d114fda28fcc8cc0d67db0aab32db1b870a1281
iskilled <iskilled@gmail.com>
2018-02-06 17:56:45 +0000

    Add NODE_ENV to webpack using define plugin

--- client/serve.js
+++ client/serve.js
@@ -12,6 +12,12 @@ const app = express();
 
 // setup hot reload
 config.plugins = [
+  // define plugin for node env
+  new webpack.DefinePlugin({
+    'process.env': {NODE_ENV: JSON.stringify(process.env.NODE_ENV)},
+  }),
+  
+  // hot reload plugin
   new webpack.HotModuleReplacementPlugin(),
   
   // setup no errors plugin
