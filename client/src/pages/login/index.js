7ce81a87e8e258328f4da3aa4259ee66fbfe7a14
iskilled <iskilled@gmail.com>
2018-02-15 19:04:27 +0000

    add first version of login page

--- client/src/pages/login/index.js
+++ client/src/pages/login/index.js
@@ -1,5 +1,21 @@
 import React from 'react';
 
+import '../../css/signin.css';
+
 export default () => (
-  <h1>Login screen</h1>
+  <form className="form-signin">
+    <h1 className="h1 mb-1 font-weight-normal">Experts portal</h1>
+    <h2 className="h3 mb-3 font-weight-normal">Please sign in</h2>
+    <label htmlFor="inputUsername" className="sr-only">Email address</label>
+    <input type="email" id="inputUsername" className="form-control" placeholder="Username" required autofocus />
+    <label htmlFor="inputPassword" className="sr-only">Password</label>
+    <input type="password" id="inputPassword" className="form-control" placeholder="Password" required />
+    <div className="checkbox mb-3">
+      <label>
+        <input type="checkbox" id="inputRemember" value="remember-me" /> Remember me
+      </label>
+    </div>
+    <button className="btn btn-lg btn-primary btn-block" type="submit">Login</button>
+    <p className="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
+  </form>
 );
