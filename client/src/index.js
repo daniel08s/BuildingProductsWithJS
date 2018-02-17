c7a82f43193d497ef6ead8d3dea01a9a99686e62
iskilled <iskilled@gmail.com>
2018-02-17 18:17:03 +0000

    add route for registration

--- client/src/index.js
+++ client/src/index.js
@@ -17,6 +17,7 @@ import {requireAuth} from './util';
 // our pages
 import Home from './pages/home';
 import Login from './pages/login';
+import Register from './pages/register';
 import NotFound from './pages/notfound';
 
 // Create an enhanced history that syncs navigation events with the store
@@ -29,6 +30,7 @@ ReactDOM.render(
       <Route path="/" component={App}>
         <IndexRoute component={Home} onEnter={requireAuth} />
         <Route path="login" component={Login} />
+        <Route path="register" component={Register} />
         <Route path="*" component={NotFound} />
       </Route>
     </Router>
