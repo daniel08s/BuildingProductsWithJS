b34c517d242c428b5250d9b0016a2da3edb84051
iskilled <iskilled@gmail.com>
2018-02-17 18:29:40 +0000

    add routerMiddleware using browserHistory for navigation

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,8 +1,10 @@
-/* eslint no-underscore-dangle: ["error", { "allow": ["__REDUX_DEVTOOLS_EXTENSION__", "_bar"] }] */
+/* eslint no-underscore-dangle: ["error", { "allow": ["__REDUX_DEVTOOLS_EXTENSION_COMPOSE__", "_bar"] }] */
 
 // npm packages
 import {createStore, applyMiddleware, compose} from 'redux';
 import {createEpicMiddleware} from 'redux-observable';
+import {browserHistory} from 'react-router';
+import {routerMiddleware} from 'react-router-redux';
 
 // our packages
 import rootReducer from './rootReducer';
@@ -11,10 +13,19 @@ import rootEpic from './rootEpic';
 // instantiate epic middleware
 const epicMiddleware = createEpicMiddleware(rootEpic);
 
+// instantiate router middleware
+const preparedRouterMiddleware = routerMiddleware(browserHistory);
+
 // pick debug or dummy enhancer
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+// apply and compose middlewares
+const middlewares = composeEnhancers(
+  applyMiddleware(epicMiddleware),
+  applyMiddleware(preparedRouterMiddleware),
+);
+
 // create store
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(epicMiddleware)));
+const store = createStore(rootReducer, middlewares);
 
 export default store;
