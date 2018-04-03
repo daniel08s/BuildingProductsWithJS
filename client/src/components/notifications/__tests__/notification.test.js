29da8071d0e4a2480e8ea2d1bebac9217aa329cf
iskilled <iskilled@gmail.com>
2018-04-03 22:21:36 +0100

    Add tests for notification components

---
+++ client/src/components/notifications/__tests__/notification.test.js
@@ -0,0 +1,34 @@
+/* global test expect shallow React */
+// npm packages
+import configureMockStore from 'redux-mock-store';
+
+// our packages
+import NotificationWrapper, {Notification} from '../notification';
+
+// create mock store
+const mockStore = configureMockStore();
+
+const notification = {
+  id: '1',
+  text: 'Test notification',
+  alertType: 'info',
+};
+
+test('# NotificationWrapper', () => {
+  const store = mockStore();
+
+  const wrapper = shallow(<NotificationWrapper notification={notification} store={store} />);
+  expect(wrapper).toMatchSnapshot();
+});
+
+test('# Notification', () => {
+  const onClick = id => expect(id).toBe(notification.id);
+  const component = <Notification notification={notification} onRemoveNotificationClick={onClick} />;
+  const wrapper = shallow(component);
+  expect(wrapper).toMatchSnapshot();
+
+  // test interaction
+  const app = mount(component);
+  const item = app.find('button');
+  item.simulate('click');
+});
