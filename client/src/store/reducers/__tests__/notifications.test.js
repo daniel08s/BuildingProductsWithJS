e633e0512a2703f29ff2a9edb80b1fba4cfb6547
iskilled <iskilled@gmail.com>
2018-04-17 00:11:11 +0100

    Add tests for notifications reducer

---
+++ client/src/store/reducers/__tests__/notifications.test.js
@@ -0,0 +1,57 @@
+/* global test expect */
+import {notifications} from '../notifications';
+import * as ActionTypes from '../../actionTypes';
+
+test('# add notification', () => {
+  const testState = [];
+  const action = {
+    type: ActionTypes.ADD_NOTIFICATION,
+    payload: {
+      id: 1,
+      text: 'Test notification!',
+      alertType: 'info',
+    },
+  };
+  expect(notifications(testState, action).length).toBe(1);
+  expect(notifications(testState, action)[0]).toBe(action.payload);
+  expect(notifications(testState, action)).toEqual([...testState, action.payload]);
+});
+
+test('# remove notification', () => {
+  const testState = [
+    {
+      id: 1,
+      text: 'Test notification!',
+      alertType: 'info',
+    },
+    {
+      id: 2,
+      text: 'Another test notification!',
+      alertType: 'info',
+    }
+  ];
+  const action = {
+    type: ActionTypes.REMOVE_NOTIFICATION,
+    payload: {
+      notificationId: 1,
+    },
+  };
+  expect(notifications(testState, action)).toEqual([testState[1]]);
+  expect(notifications(testState, action).length).toBe(1);
+});
+
+test('# notifications reducer - errors', () => {
+  const testState = [
+    {
+      id: 1,
+      text: 'Test notification!',
+      alertType: 'info',
+    },
+    {
+      id: 2,
+      text: 'Another test notification!',
+      alertType: 'info',
+    }
+  ];
+  expect(notifications(testState, {type: -1})).toEqual(testState);
+});
