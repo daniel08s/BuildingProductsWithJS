c12d23efa4db544aa682bec38cd8b208fee31a08
iskilled <iskilled@gmail.com>
2018-04-23 20:56:39 +0100

    Add tests for auth epic (registration)

--- client/src/store/epics/__tests__/auth.test.js
+++ client/src/store/epics/__tests__/auth.test.js
@@ -66,3 +66,55 @@ test('# login epic - login error', () => {
       }
     });
 });
+
+test('# register epic - registration success', () => {
+  const payload = {test: true};
+  const response = {data: true};
+  const action$ = ActionsObservable.from([{type: ActionTypes.DO_REGISTER, payload}]);
+  const post = jest.fn().mockReturnValueOnce(Observable.from([{response}]));
+  Observable.ajax.post = post;
+
+  let responseCount = 0;
+  register(action$)
+    .subscribe((res) => {
+      if (!responseCount) {
+        expect(post.mock.calls.length).toBe(1);
+        expect(post.mock.calls[0][0]).toBe('http://localhost:8080/api/register');
+        expect(post.mock.calls[0][1]).toEqual(payload);
+        expect(res).toEqual({type: 'REGISTER_SUCCESS', payload: {...response}});
+        responseCount += 1;
+      } else {
+        expect(res).toEqual({
+          type: 'ADD_NOTIFICATION',
+          payload: {
+            id: 1,
+            text: 'Registration successful!',
+            alertType: 'info',
+          },
+        });
+      }
+    });
+});
+
+test('# register epic - registration error', () => {
+  const action$ = ActionsObservable.from([{type: ActionTypes.DO_REGISTER, payload: {}}]);
+
+  let responseCount = 0;
+  register(action$)
+    .subscribe((res) => {
+      if (!responseCount) {
+        expect(res.type).toBe('REGISTER_ERROR');
+        expect(res.payload.error.AjaxError.message).toBe('ajax error');
+        responseCount += 1;
+      } else {
+        expect(res).toEqual({
+          type: 'ADD_NOTIFICATION',
+          payload: {
+            id: 1,
+            text: 'ajax error',
+            alertType: 'danger',
+          },
+        });
+      }
+    });
+});
